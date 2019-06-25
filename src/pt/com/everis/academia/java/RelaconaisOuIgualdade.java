cf5abbe726a10d7fea7b47234b841b7c2b6bc56c
Ricardo do Canto <endurancecode@gmail.com>
2019-06-25 17:14:26 +0100

    Equalty operators

---
+++ src/pt/com/everis/academia/java/RelaconaisOuIgualdade.java
@@ -0,0 +1,32 @@
+package pt.com.everis.academia.java;
+
+public class RelaconaisOuIgualdade {
+
+	public static void main(String[] args) {
+
+		int value1 = 1;
+		int value2 = 2;
+		
+		if (value1 == value2) {
+			System.out.println(false);
+		}
+		
+		if (value1 != value2) {
+			System.out.println(true);
+		}
+		
+		if (value1 > value2) {
+			System.out.println(false);
+		}
+		
+		if (value1 < value2) {
+			System.out.println(true);
+		}
+		
+		if (value1 <= value2) {
+			System.out.println(true);
+		}
+
+	}
+
+}
