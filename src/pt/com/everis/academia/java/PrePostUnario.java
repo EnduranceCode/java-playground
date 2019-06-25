b82d30bf2de602d61b18afcf4a2d1f1ceeac6122
Ricardo do Canto <endurancecode@gmail.com>
2019-06-25 17:13:51 +0100

    Pre and Post Unitary operators

---
+++ src/pt/com/everis/academia/java/PrePostUnario.java
@@ -0,0 +1,21 @@
+package pt.com.everis.academia.java;
+
+public class PrePostUnario {
+
+	public static void main(String[] args) {
+
+		int i = 3;
+		i++;
+		
+		System.out.println(i);
+		++i;
+		
+		System.out.println(i);
+		
+		System.out.println(++i);
+		
+		System.out.println(i++);
+		
+		System.out.println(i++);
+	}
+}
