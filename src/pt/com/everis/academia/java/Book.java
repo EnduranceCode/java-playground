577712be1d18f2122779c773fb14ec2e3cd408f4
Ricardo do Canto <endurancecode@gmail.com>
2019-06-25 15:47:23 +0100

    Equals exercise - Second Step

--- src/pt/com/everis/academia/java/Book.java
+++ src/pt/com/everis/academia/java/Book.java
@@ -19,6 +19,33 @@ public class Book {
 		this.isbn = isbn;
 	}
 	
+	@Override
+	public boolean equals(Object obj) {
+		
+		if (this == obj) {
+			return true;
+		}
+		
+		if (obj == null) {
+			return false;
+		}
+		
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+	
+		Book other = (Book) obj;
+		if (isbn == null) {
+			
+			if (other.isbn != null) {
+				return false;
+			}
+		} else if (!isbn.equals(other.isbn)) {
+			return false;
+		}
+		return true;
+	}
+
 	public static void main(String[] args) {
 		
 		Book book1 = new Book("62");
