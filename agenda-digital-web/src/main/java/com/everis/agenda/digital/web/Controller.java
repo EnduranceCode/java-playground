2146c082082f349f4207e1e19505c0feec2a7791
Ricardo do Canto <endurancecode@gmail.com>
2019-07-01 10:59:14 +0100

    Add first form

---
+++ agenda-digital-web/src/main/java/com/everis/agenda/digital/web/Controller.java
@@ -0,0 +1,36 @@
+package com.everis.agenda.digital.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "controller", urlPatterns = "/controller")
+public class Controller extends HttpServlet {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String nome = request.getParameter("nome");
+		Integer idade = Integer.valueOf(request.getParameter("idade"));
+		Double salario = Double.valueOf(request.getParameter("salario"));
+		
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body bgcolor=\"#FF0000\">");
+		out.println("<table border=\"1\">");
+		out.println("<tr><td colspan=\"2\">Parâmetros Recebidos</td></tr>");
+		out.println("<tr><td>Nome:</td><td>" + nome + "</td></tr>");
+		out.println("<tr><td>Idade:</td><td>" + idade + "</td></tr>");
+		out.println("<tr><td>Salario:</td><td>" + salario + "</td></tr>");
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
