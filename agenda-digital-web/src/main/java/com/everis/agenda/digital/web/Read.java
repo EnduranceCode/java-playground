8c74838c61e74226e9e9a70eccbf2aeca68b8db6
Ricardo do Canto <endurancecode@gmail.com>
2019-07-01 19:16:04 +0100

    Add Read Servlet

--- agenda-digital-web/src/main/java/com/everis/agenda/digital/web/Read.java
+++ agenda-digital-web/src/main/java/com/everis/agenda/digital/web/Read.java
@@ -13,19 +13,13 @@ import com.everis.academia.java.agenda.digital.entity.Cidade;
 import com.everis.agenda.digital.web.blocks.HeadHtml;
 import com.everis.agenda.digital.web.storage.Storage;
 
-@WebServlet(name = "nova-cidade", urlPatterns = "/nova-cidade")
-public class NovaCidade extends HttpServlet {
+@WebServlet(name = "read", urlPatterns = "/read")
+public class Read extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Override
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		/* Obtêm o nome da cidade inserido no formulário */
-		String cidade = request.getParameter("cidade");
-		
-		/* Insere a nova cidade na lista de armazenamento*/
-		Cidade cidadeNova = Storage.inserirCidade(cidade);
 		
 		PrintWriter out = response.getWriter();
 		out.println(new HeadHtml().getHead());
@@ -40,19 +34,25 @@ public class NovaCidade extends HttpServlet {
 		
 		out.println("<table class=\"table\">");
 		out.println("<thead>");
-		out.println("<tr><th>Codigo</th><th>Cidade</th></tr>");
+		out.println("<tr><th>Codigo</th><th>Cidade</th><th></th></tr>");
 		out.println("</thead>");
 		out.println("<tbody>");
-		out.println("<tr><td>" + cidadeNova.getCodigo() + "</td><td>"+ cidadeNova.getNome() + "</td></tr>");
+
+		for (Cidade cidadeActual : Storage.getCidades()) {
+			out.println("<tr><td>" + cidadeActual.getCodigo() + "</td><td>"+ cidadeActual.getNome() + "</td><td><a style=\"margin-right: 10px;\" class=\"btn btn-small btn-success\">Editar</a><a class=\"btn btn-small btn-danger\">Eliminar</a></td></tr>");
+		}
+		
 		out.println("</tbody>");
 		out.println("</table>");
 		
-		out.println("<p><a class=\"btn\" style=\"margin-right: 10px;\" href=\"/agenda-digital-web/create\">Inserir nova cidade</a><a class=\"btn btn-primary\" href=\"/agenda-digital-web/read\">Ver todas as cidades</a></p>");
+		out.println("<p><a class=\"btn\" href=\"/agenda-digital-web/create\">Inserir nova cidade</a></p>");
 		
 		out.println("</div>");
 		out.println("</div>");
 		
 		out.println("</body>");
 		out.println("</html>");
+		
 	}
+
 }
