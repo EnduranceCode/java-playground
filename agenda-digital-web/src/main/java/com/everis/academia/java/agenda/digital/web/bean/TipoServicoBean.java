7019f71e4b74653623a31b6bd74f8ff3550c0ac7
Ricardo do Canto <endurancecode@gmail.com>
2019-07-13 22:42:44 +0100

    Implement TipoServico's create View and Bean

---
+++ agenda-digital-web/src/main/java/com/everis/academia/java/agenda/digital/web/bean/TipoServicoBean.java
@@ -0,0 +1,59 @@
+package com.everis.academia.java.agenda.digital.web.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.everis.academia.agenda.digital.business.BusinessException;
+import com.everis.academia.agenda.digital.business.inter.ITipoServicoBusiness;
+import com.everis.academia.java.agenda.digital.entity.TipoServico;
+
+@ManagedBean(name = "tipoServicoBean")
+@Component
+@RequestScoped
+public class TipoServicoBean {
+
+	/* Instanciamos um novo objecto TipoServicoBusiness */
+	@Autowired
+	private ITipoServicoBusiness tipoServicoBusiness;
+
+	/* Instanciamos um novo objecto TipoServico para receber os dados do Frontend */
+	private TipoServico tipoServico = new TipoServico();
+
+	public TipoServico getTipoServico() {
+		return tipoServico;
+	}
+
+	public void setTipoServico(TipoServico tipoServico) {
+		this.tipoServico = tipoServico;
+	}
+
+	/**
+	 * Insere um novo Tipo de Serviçp na lista de Tipos de Serviço existentes
+	 * 
+	 * @return
+	 */
+	public String submeterTipoServico() {
+
+		try {
+
+			tipoServico = tipoServicoBusiness.create(tipoServico);
+
+			/* Re-instanciamos a variavel para limpar o formulário no Frontend */
+			tipoServico = new TipoServico();
+
+			return "create?faces-redirect=true";
+		} catch (BusinessException e) {
+
+			String messageDetails = e.getLocalizedMessage();
+			FacesContext.getCurrentInstance().addMessage(null,
+					new FacesMessage(FacesMessage.SEVERITY_ERROR, "Ocorreu um erro!", messageDetails));
+		}
+
+		return null;
+	}
+}
