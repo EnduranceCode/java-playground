bad3d50e721ff9b8f33e6aac5b5704bedb910899
Ricardo do Canto <endurancecode@gmail.com>
2019-07-15 15:37:08 +0100

    Add @Transient to variable of type "PrestadorServico" on "Telephone" class

--- agenda-digital-entity/src/main/java/com/everis/academia/java/agenda/digital/entity/Telefone.java
+++ agenda-digital-entity/src/main/java/com/everis/academia/java/agenda/digital/entity/Telefone.java
@@ -9,6 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "TB_TELEFONE", schema = "public")
@@ -30,6 +31,7 @@ public class Telefone implements Serializable {
 		super();
 	}
 	
+	@Transient
 	private PrestadorServico prestadorServico;
 
 	public Telefone(Integer codigo) {
