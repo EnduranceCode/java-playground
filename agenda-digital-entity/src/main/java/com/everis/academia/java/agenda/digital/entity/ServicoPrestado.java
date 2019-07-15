9ee6f32f1c2a1ef52316d65b22463854ceec5b43
Ricardo do Canto <endurancecode@gmail.com>
2019-07-15 15:01:11 +0100

    Add hashCode() and equals() methods

--- agenda-digital-entity/src/main/java/com/everis/academia/java/agenda/digital/entity/ServicoPrestado.java
+++ agenda-digital-entity/src/main/java/com/everis/academia/java/agenda/digital/entity/ServicoPrestado.java
@@ -49,4 +49,29 @@ public class ServicoPrestado {
 	public void setValor(Float valor) {
 		this.valor = valor;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ServicoPrestado other = (ServicoPrestado) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
 }
