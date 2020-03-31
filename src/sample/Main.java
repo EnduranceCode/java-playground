774872fc89186564e64d379791ae7e354ac0ed92
Ricardo do Canto <endurancecode@gmail.com>
2020-03-31 16:06:29 +0100

    Style the Text

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -31,7 +31,7 @@ public class Main extends Application {
         grid.setPadding(new Insets(25, 25, 25, 25));
 
         Text scenetitle = new Text("Welcome");
-        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        scenetitle.setId("welcome-text");
         grid.add(scenetitle, 0, 0, 2, 1);
 
         Label userName = new Label("User Name:");
@@ -60,16 +60,7 @@ public class Main extends Application {
 
             @Override
             public void handle(ActionEvent e) {
-                actiontarget.setFill(Color.FIREBRICK);
-                actiontarget.setText("Sign in button pressed");
-            }
-        });
-
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent e) {
-                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setId("actiontarget");
                 actiontarget.setText("Sign in button pressed");
             }
         });
