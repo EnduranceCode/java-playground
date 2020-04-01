1799bcdf311900c6f54fbda2286ddb64a6b60aca
Ricardo do Canto <endurancecode@gmail.com>
2020-04-01 15:53:11 +0100

    Initialize "Animated Shapes and Visual Effects"

---
+++ src/tutorial/ColorfulCircles.java
@@ -0,0 +1,23 @@
+package tutorial;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class ColorfulCircles extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 800, 600, Color.BLACK);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
+    }
+}
