b12fa29d85bc0ebd333abc997d207fcab58edab2
Ricardo do Canto <endurancecode@gmail.com>
2020-04-01 08:56:28 +0100

    Load FXML file and refactor stage title

--- src/fxmlexample/FXMLExample.java
+++ src/fxmlexample/FXMLExample.java
@@ -11,8 +11,11 @@ public class FXMLExample extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+
+        Scene scene = new Scene(root, 300, 275);
+
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
