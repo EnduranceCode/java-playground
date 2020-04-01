e1f96d4acdfdda56398ea1f09607c03265e7e6ec
Ricardo do Canto <endurancecode@gmail.com>
2020-04-01 16:51:32 +0100

    Create a Background Gradient

--- src/tutorial/ColorfulCircles.java
+++ src/tutorial/ColorfulCircles.java
@@ -5,7 +5,11 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.Stop;
 import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
@@ -29,8 +33,23 @@ public class ColorfulCircles extends Application {
             circle.setStrokeWidth(4);
             circles.getChildren().add(circle);
         }
-        root.getChildren().add(circles);
 
+        Rectangle colors = new Rectangle(scene.getWidth(), scene.getHeight(),
+                new LinearGradient(0f, 1f, 1f, 0f, true, CycleMethod.NO_CYCLE, new
+                        Stop[]{
+                        new Stop(0, Color.web("#f8bd55")),
+                        new Stop(0.14, Color.web("#c0fe56")),
+                        new Stop(0.28, Color.web("#5dfbc1")),
+                        new Stop(0.43, Color.web("#64c2f8")),
+                        new Stop(0.57, Color.web("#be4af7")),
+                        new Stop(0.71, Color.web("#ed5fc2")),
+                        new Stop(0.85, Color.web("#ef504c")),
+                        new Stop(1, Color.web("#f2660f")),}));
+        colors.widthProperty().bind(scene.widthProperty());
+        colors.heightProperty().bind(scene.heightProperty());
+
+        root.getChildren().add(colors);
+        root.getChildren().add(circles);
         circles.setEffect(new BoxBlur(10, 10, 3));
 
         primaryStage.show();
