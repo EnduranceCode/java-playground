534adf9b88500387c3bb136c7e2ef96b2f89ea12
Ricardo do Canto <endurancecode@gmail.com>
2020-04-01 16:38:31 +0100

    Add Graphics

--- src/tutorial/ColorfulCircles.java
+++ src/tutorial/ColorfulCircles.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
 
 public class ColorfulCircles extends Application {
@@ -17,6 +19,16 @@ public class ColorfulCircles extends Application {
         Group root = new Group();
         Scene scene = new Scene(root, 800, 600, Color.BLACK);
         primaryStage.setScene(scene);
+
+        Group circles = new Group();
+        for (int i = 0; i < 30; i++) {
+            Circle circle = new Circle(150, Color.web("white", 0.05));
+            circle.setStrokeType(StrokeType.OUTSIDE);
+            circle.setStroke(Color.web("white", 0.16));
+            circle.setStrokeWidth(4);
+            circles.getChildren().add(circle);
+        }
+        root.getChildren().add(circles);
 
         primaryStage.show();
     }
