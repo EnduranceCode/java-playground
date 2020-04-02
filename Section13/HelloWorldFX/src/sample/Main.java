1dd10317d5f652783554749f484d9b1eb6bc6420
Ricardo do Canto <endurancecode@gmail.com>
2020-04-02 20:55:16 +0100

    S13:182 Create view with FXML

--- Section13/HelloWorldFX/src/sample/Main.java
+++ Section13/HelloWorldFX/src/sample/Main.java
@@ -1,13 +1,9 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 
@@ -16,15 +12,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
 
-        GridPane root = new GridPane();
-        root.setAlignment(Pos.CENTER);
-        root.setVgap(10);
-        root.setHgap(10);
-
-        Label greeting = new Label("Welcome to JavaFX!");
-        greeting.setTextFill(Color.GREEN);
-        greeting.setFont(Font.font("Arial",FontWeight.BOLD, 70));
-        root.getChildren().add(greeting);
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
         primaryStage.setTitle("Hello JavaFX!");
         primaryStage.setScene(new Scene(root, 825, 275));
