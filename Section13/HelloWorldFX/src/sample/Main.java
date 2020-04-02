addcf841230c592f2fa926eaf1fa7ffd460c9de9
Ricardo do Canto <endurancecode@gmail.com>
2020-04-02 20:38:39 +0100

    S13:182 Create view with Java code

--- Section13/HelloWorldFX/src/sample/Main.java
+++ Section13/HelloWorldFX/src/sample/Main.java
@@ -1,21 +1,35 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+
+        GridPane root = new GridPane();
+        root.setAlignment(Pos.CENTER);
+        root.setVgap(10);
+        root.setHgap(10);
+
+        Label greeting = new Label("Welcome to JavaFX!");
+        greeting.setTextFill(Color.GREEN);
+        greeting.setFont(Font.font("Arial",FontWeight.BOLD, 70));
+        root.getChildren().add(greeting);
+
+        primaryStage.setTitle("Hello JavaFX!");
+        primaryStage.setScene(new Scene(root, 825, 275));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
