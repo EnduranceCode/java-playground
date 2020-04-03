e5f84feefb5adf16f8c58edf7f34c970f62fd184
Ricardo do Canto <endurancecode@gmail.com>
2020-04-03 19:21:38 +0100

    Add an action to the Print button

--- exercise.001/src/exercise/one/OnePresenter.java
+++ exercise.001/src/exercise/one/OnePresenter.java
@@ -2,6 +2,7 @@ package exercise.one;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
@@ -12,7 +13,16 @@ public class OnePresenter {
     private Text outputHeaderTextFieldValue;
 
     @FXML
-    protected void headerSubmitButtonAction(ActionEvent actionEvent) {
+    protected void headerSubmitButtonAction() {
         outputHeaderTextFieldValue.setText(headerTextField.getText());
+    }
+
+    @FXML
+    protected void bodyPrintButtonAction() {
+        Alert alertPopUp = new Alert(Alert.AlertType.INFORMATION);
+        alertPopUp.setTitle("Print Button Pop Up Window");
+        alertPopUp.setHeaderText("The Print Button has been clicked!");
+        alertPopUp.setContentText("This Pop Up window appears when the Print button is clicked. Press the OK button to close this Pop Up window");
+        alertPopUp.showAndWait();
     }
 }
