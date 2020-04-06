c40b46977e9a32fa133cb7df874f9f969db38364
Ricardo do Canto <endurancecode@gmail.com>
2020-04-06 15:42:35 +0100

    Add an action to the "Back to the Main Screen" button

--- exercise.001/src/exerciseone/secondscreen/SecondScreenPresenter.java
+++ exercise.001/src/exerciseone/secondscreen/SecondScreenPresenter.java
@@ -1,4 +1,24 @@
 package exerciseone.secondscreen;
 
+import exerciseone.firstscreen.FirstScreenView;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class SecondScreenPresenter {
+    @FXML
+    Button backToMainScreen;
+
+    @FXML
+    protected void showMainScreen() {
+        Stage currentStage = (Stage) backToMainScreen.getScene().getWindow();
+        currentStage.setTitle("JavaFX Exercise One");
+
+        FirstScreenView firstScreenView = new FirstScreenView();
+
+        Scene firstScene = new Scene(firstScreenView.getView());
+
+        currentStage.setScene(firstScene);
+    }
 }
