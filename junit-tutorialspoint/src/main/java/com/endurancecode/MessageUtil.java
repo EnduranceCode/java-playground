e32321430a7d2092112cdce9a5d9e12843643a01
Ricardo do Canto <endurancecode@gmail.com>
2021-10-21 12:07:54 +0100

    Tutorialspoint Junit - Basic usage

---
+++ junit-tutorialspoint/src/main/java/com/endurancecode/MessageUtil.java
@@ -0,0 +1,21 @@
+package com.endurancecode;
+
+/**
+ * Tutorialspoint: Learn Junit
+ * https://www.tutorialspoint.com/junit/
+ */
+public class MessageUtil {
+    private String message;
+
+    // Constructor
+    // @param message to be printed
+    public MessageUtil(String message){
+        this.message = message;
+    }
+
+    // Prints the message
+    public String printMessage(){
+        System.out.println(message);
+        return message;
+    }
+}
--- junit-tutorialspoint/src/test/java/com/endurancecode/tutorialspoint/environment/TestJunit.java
+++ junit-tutorialspoint/src/test/java/com/endurancecode/tutorialspoint/environment/TestJunit.java
@@ -4,6 +4,13 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+/**
+ * Tutorialspoint: Learn Junit
+ * https://www.tutorialspoint.com/junit/
+ *
+ * Chapter: JUnit - Environment Setup
+ * https://www.tutorialspoint.com/junit/junit_environment_setup.htm
+ */
 public class TestJunit {
     @Test
     public void testAdd() {
