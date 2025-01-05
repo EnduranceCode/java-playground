eec47cf3aa6fcf52736a3aafafa5ce9781e6cec2
Ricardo do Canto <endurancecode@gmail.com>
2025-01-05 22:22:57 +0000

    Solve Exercise 6 from Chapter 4

---
+++ exam-1z0-811/enthuware/book-chapter-04/exercise-05/ExerciseSix.java
@@ -0,0 +1,23 @@
+public class ExerciseSix {
+
+	int loop;
+
+	String lastName;
+
+	Double average;
+
+	final int TaxRate = 6;
+
+	void findPrime() {
+	}
+
+	void computeMean(int[] marksObtained) {}
+}
+
+class Manager {
+}
+
+class FullTimeEmployee{
+}
+
+
