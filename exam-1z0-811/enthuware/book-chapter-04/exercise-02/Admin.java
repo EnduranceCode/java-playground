b8af913d959ddec885741488f0ef4c8360c00fda
Ricardo do Canto <endurancecode@gmail.com>
2025-01-05 21:59:17 +0000

    Solve Exercise 2 from Chapter 4

---
+++ exam-1z0-811/enthuware/book-chapter-04/exercise-02/Admin.java
@@ -0,0 +1,12 @@
+package hr;
+
+// import hr.reporting.*;
+// import hr.reporting.TimeCard;
+
+public class Admin {
+
+	public static void main(String[] args) {
+		// TimeCard.add();
+		hr.reporting.TimeCard.add();
+	}
+}
---
+++ exam-1z0-811/enthuware/book-chapter-04/exercise-02/TimeCard.java
@@ -0,0 +1,7 @@
+package hr.reporting;
+
+public class TimeCard {
+	public static void add() {
+		System.out.println("The method hr.reporting.TimeCard.add was invoked");
+	}
+}
